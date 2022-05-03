5925f395bebc2ca781b8682d32fa67f17fe69e5f
MattiaDelCucina03 <103566073+MattiaDelCucina03@users.noreply.github.com>
2022-05-03 11:27:34 +0200

    Delete AjaxCall.js

--- GameLauncher/launcher/MiniGames/AjaxCall.js
+++
@@ -1,9 +0,0 @@
-export function addScore(gameCode,score){
-    var xhttp=new XMLHttpRequest();
-    /*xhttp.onreadystatechange = function() {
-        console.log(this.status);
-        console.log(this.responseText);
-      };*/
-    xhttp.open("GET", "../AjaxCall.php?gameCode="+gameCode+"&score="+score, true);
-    xhttp.send();
-}
