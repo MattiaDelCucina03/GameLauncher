9f7118da035665abdf8c5f965b704db031e64a4f
LucasItis <103572764+LucasItis@users.noreply.github.com>
2022-05-05 11:23:16 +0200

    Modifica della classifica. File modificati: "ShowRanks cartella Ajax", "Styles.css", correzione file "login.php"

--- GameLauncher/launcher/Ajax/PHP/AjaxShowRanks.php
+++ GameLauncher/launcher/Ajax/PHP/AjaxShowRanks.php
@@ -14,22 +14,49 @@ if($gameCode=="GENERAL"){
 $result=$mysqli->query($query);
 $posizione=0;
 ?>
-<table class="table table-striped">
+<div class="container podium">
+    <?php
+    while($row=$result->fetch_assoc()){
+        $posizione = $posizione +1;
+        if($posizione == 1){
+            echo '<div class="podium__item">
+            <p class="podium__city">'.$row["name"].' score: '.$row["score"].'</p>
+            <div class="podium__rank first">
+              <svg class="podium__number" viewBox="0 0 27.476 75.03" xmlns="http://www.w3.org/2000/svg">
+              <g transform="matrix(1, 0, 0, 1, 214.957736, -43.117417)">
+                <path class="st8" d="M -198.928 43.419 C -200.528 47.919 -203.528 51.819 -207.828 55.219 C -210.528 57.319 -213.028 58.819 -215.428 60.019 L -215.428 72.819 C -210.328 70.619 -205.628 67.819 -201.628 64.119 L -201.628 117.219 L -187.528 117.219 L -187.528 43.419 L -198.928 43.419 L -198.928 43.419 Z" style="fill: #000;"/>
+              </g>
+            </svg>
+            </div>
+        </div>';
+        } else if($posizione == 2){
+            echo '<div class="podium__item">
+            <p class="podium__city">'.$row["name"].' score: '.$row["score"].'</p>
+            <div class="podium__rank second">2</div>
+          </div>';
+        } else if($posizione == 3){
+            echo '<div class="podium__item">
+            <p class="podium__city">'.$row["name"].' score: '.$row["score"].'</p>
+            <div class="podium__rank third">3</div>
+          </div>';
+        } else if($posizione > 15){
+            
+        } else {
+    ?>
+</div>
+<table>
     <tr>
         <th>Posizione</th>
         <th>Utente</th>
         <th>Punti</th>
     </tr>
-    <?php
-    while($row=$result->fetch_assoc()){
-        $posizione = $posizione +1;
+<?php
         echo "<tr>";
         echo "<td>".$posizione."</td>";
         echo "<td>".$row["name"]."</td>";
         echo "<td>".$row["score"]."</td>";
         echo "</tr>";
-    }
-    ?>
-</table>
-<?php
+        }
+}
 ?>
+</table>
